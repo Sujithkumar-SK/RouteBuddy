c44a1e69c0e5b9728966118aec312e1bf550caba
Sujith Kumar S <sujinano777@gmail.com>
2025-09-07 14:12:42 +0530

    dbrelation

--- SmartEngine/SmartEngine/obj/Debug/net9.0/SmartEngine.AssemblyInfo.cs
+++ SmartEngine/SmartEngine/obj/Debug/net9.0/SmartEngine.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartEngine")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a666e2fbc4172aa5fbec70fc4a147e830468c0f")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartEngine")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartEngine")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
